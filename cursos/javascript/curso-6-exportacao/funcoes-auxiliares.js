4dd712285ef90cf4608a4efce8097086755314e4
Bruno Alejandro <brunoalejandrodev@gmail.com>
2024-03-17 13:05:29 -0300

    exercício sobre import e export

---
+++ cursos/javascript/curso-6-exportacao/funcoes-auxiliares.js
@@ -0,0 +1,15 @@
+// local para colocar as funções que serão exportadas
+const numeros = [5, 50, 10, 98, 23];
+let iterador = 0;
+
+function gets() {
+    const valor = numeros[iterador];
+    iterador++;
+    return valor
+}
+
+function print(texto) {
+    console.log(texto);
+}
+ 
+module.exports = {gets, print}
---
+++ cursos/javascript/curso-6-exportacao/numero-sorteado.js
@@ -0,0 +1,28 @@
+/*
+    Utilizando o conceito de importação e exportação, resolva o seguinte problema: uma sala contém 5 alunos e para cada aluno foi sorteado um número de 1 a 100. Faça um programa que receba os números sorteados e mostre o maior entre eles.
+
+    números: 5, 50, 10, 98, 23
+*/
+const { gets, print } = require("./funcoes-auxiliares");
+
+// array para armazenar os números sorteados
+const numerosSorteados = [];
+
+// loop para percorrer o array com os números que está no arquivo de funções
+for (let i = 0; i < 5; i++) {
+  const numeroSorteado = gets(); // salvei cada número na variável
+  numerosSorteados.push(numeroSorteado); // estou colocando cada número no array
+}
+print(numerosSorteados);
+
+// loop para percorrer o array numerosSorteados e verificar qual o maior número
+
+let maiorValor = 0;
+
+for (let i = 0; i < numerosSorteados.lengt; i++) {
+  const numeroSorteado = numerosSorteados[i];
+  if (numeroSorteado > maiorValor) {
+    maiorValor = numeroSorteado;
+  }
+}
+print(maiorValor);
