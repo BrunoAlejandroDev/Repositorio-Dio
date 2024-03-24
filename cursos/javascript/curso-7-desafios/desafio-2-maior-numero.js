5db9ca19c2dd703deb686e2d0f4142be6fe93242
Bruno Alejandro <brunoalejandrodev@gmail.com>
2024-03-24 19:25:13 -0300

    curso 7 - desafio 2 - maior e menor numero

---
+++ cursos/javascript/curso-7-desafios/desafio-2-maior-numero.js
@@ -0,0 +1,27 @@
+/*
+    Desafio 2: faça um programa que receba uma quantidade N de números e seus respectivos valores. Imprima o maior número par e o menor número impar.
+*/
+
+const {gets, print} = require('./funcoes-auxiliares-2');
+const n = gets();
+let maiorNumPar = 0;
+let menorNumImpar = 0;
+
+for(let i = 0; i < n; i++) {
+    const numero = gets();
+    if(numero % 2 === 0) {
+        if(numero > maiorNumPar) {
+            maiorNumPar = numero
+        }
+    }
+    else {
+        if(menorNumImpar === 0) {
+            menorNumImpar = numero;
+        }
+        else if (numero < menorNumImpar) {
+            menorNumImpar = numero;
+        }
+    }
+}
+print(maiorNumPar)
+print(menorNumImpar)
