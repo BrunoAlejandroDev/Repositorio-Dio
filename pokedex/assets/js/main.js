0f31922f06b0bad6976b8b72fc78f7899d518ad1
Bruno Alejandro <brunoalejandrodev@gmail.com>
2024-04-23 15:36:01 -0300

    executando uma requisição para a API usando fetch

--- pokedex/assets/js/main.js
+++ pokedex/assets/js/main.js
@@ -1,1 +1,15 @@
-console.log('sucesso');
+const offset = 0;
+const limit = 10;
+
+const urlPokemon = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+fetch(urlPokemon)
+    .then(function (response){ // serve para executar algo quando a requisição for atendida
+        console.log(response);
+    })
+    .catch(function (error){ // serve para executar algo quando a requisição der erro
+        console.log(error);
+    })
+    .finally(function (){ // serve para executar um comando independente da requisição ter funcionado ou não
+        console.log('Requisição finalizada');
+    })
