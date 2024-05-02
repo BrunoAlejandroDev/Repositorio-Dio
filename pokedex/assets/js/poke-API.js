af32d6aefd1d17ab98905ebac683d289cc637472
Bruno Alejandro <brunoalejandrodev@gmail.com>
2024-05-02 10:49:48 -0300

    js: lidando com múltiplas requisições em paralelo

--- pokedex/assets/js/poke-API.js
+++ pokedex/assets/js/poke-API.js
@@ -1,14 +1,26 @@
+//? serve para executar algo quando a requisição for atendida
+//? then encadeado: serve para executar algo a partir do resultado da execução do then anterior
+
 //* criação de um objeto para receber a requisição
 const pokeAPI = {};
 
 //* criando um método (getPokemons) que retorna o resultado da requisição
-pokeAPI.getPokemons = (offset = 0, limit = 10) => {
+pokeAPI.getPokemons = (offset = 0, limit = 5) => {
     const urlPokemon = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
     return fetch(urlPokemon)
-        .then((response) => response.json()) //? serve para executar algo quando a requisição for atendida
+        .then((response) => response.json()) 
+        .then((jsonBody) => jsonBody.results)
+        .then((pokemons) => pokemons.map(pokeAPI.getPokemonDetails))
+        .then((detailsRequired) => Promise.all(detailsRequired))
+        .then((pokemonsDetails) => {
+            console.log(pokemonsDetails);
+        }) 
 
-        .then((jsonBody) => jsonBody.results) //? then encadeado: serve para executar algo a partir do resultado da execução do then anterior
+        .catch((error) => console.log(error))
+}
 
-        .catch((error) => console.log(error)) //? define uma execução no caso de erro
+//* criando um método para, por meio da URL retornada na requisição anterior, pegar os detalhes do pokemon
+pokeAPI.getPokemonDetails = (pokemon) => {
+    return fetch(pokemon.url).then((response) => response.json());
 }
