16f2ab19c21ff2af49048017a8165cc647e7505e
Bruno Alejandro <brunoalejandrodev@gmail.com>
2024-05-07 15:12:37 -0300

    js: criado função no btn para gerar mais pokemons

--- pokedex/assets/js/main.js
+++ pokedex/assets/js/main.js
@@ -1,31 +1,34 @@
-function convertListPokemonToListItem (pokemon) {
-    return `
-        <li class="list-pokemons__pokemon ${pokemon.type}">
-            <span class="pokemon-number">#${pokemon.number}</span>
-            <span class="pokemon-name">${pokemon.name}</span>
-            
-            <div class="pokemon-details">
-                <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join("")}
-                </ol>
-                
-                <img 
-                    src="${pokemon.photo}"
-                    alt="${pokemon.name}"
-                />
-            </div>
-        </li>
-    `
-}
-
+//* Elementos HTML
 const pokemonsList = document.getElementById('pokemons-list');
+const btnLoadMore = document.getElementById('btn-load-more');
+const limit = 5;
+let offset = 0;
 
-pokeAPI.getPokemons().then((pokemonList = []) => {
-    pokemonsList.innerHTML += pokemonList.map(convertListPokemonToListItem).join("");
-}) 
+function loadPokemons(offset, limit) {
+    pokeAPI.getPokemons(offset, limit).then((pokemonList = []) => {
+        pokemonsList.innerHTML += pokemonList.map((pokemon) =>
+            `<li class="list-pokemons__pokemon ${pokemon.type}">
+                <span class="pokemon-number">#${pokemon.number}</span>
+                <span class="pokemon-name">${pokemon.name}</span>
+                
+                <div class="pokemon-details">
+                    <ol class="types">
+                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join("")}
+                    </ol>
+                    
+                    <img 
+                        src="${pokemon.photo}"
+                        alt="${pokemon.name}"
+                    />
+                </div>
+            </li>`
+        ).join("");
+    }) 
+}
+loadPokemons(offset, limit)
 
-//* O map acima faz a mesma coisa que o bloco de código abaixo
-// for (let i = 0; i < pokemonList.length; i++) {
-//     const pokemon = pokemonList[i];
-//     pokemonsList.innerHTML += convertListPokemonToListItem(pokemon);
-// }
+//* criando interatividade do botão
+btnLoadMore.addEventListener('click', () => {
+    offset += limit;
+    loadPokemons(offset, limit)
+});
