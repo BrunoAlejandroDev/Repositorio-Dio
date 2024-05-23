3c8f7b84a18396ce446f8617971284b70561fac0
Bruno Alejandro <brunoalejandrodev@gmail.com>
2024-05-23 16:00:01 -0300

    inicio aulas de funções e listas

---
+++ cursos/javascript/curso9-funcoes-e-listas/aula1.js
@@ -0,0 +1,10 @@
+function falarMeuNome() {
+    console.log('Meu nome é bruno');
+}
+
+function falarMeuNomeCompleto(falarMeuNome) {
+    falarMeuNome()
+    console.log('alejandro gonzalez pimentel');
+}
+
+falarMeuNomeCompleto(falarMeuNome)
